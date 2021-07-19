c9438abd35b5f825940eb78d9ca3c709da978554
Alex Woolford <alex@woolford.io>
2021-07-18 20:38:06 -0600

    bumped version, added props for Confluent Cloud

--- src/main/java/io/woolford/KafkaConstants.java
+++ src/main/java/io/woolford/KafkaConstants.java
@@ -2,7 +2,10 @@ package io.woolford;
 
 public interface KafkaConstants {
 
-    public static String KAFKA_BROKERS = "cp01.woolford.io:9092,cp02.woolford.io:9092,cp03.woolford.io:9092";
-    public static String APPLICATION_ID = "snowplow-kafka-streams";
+    public static String KAFKA_BROKERS = "pkc-lzvrd.us-west4.gcp.confluent.cloud:9092";
+    public static String APPLICATION_ID = "snowplow-kafka-streams-1";
+    public static String SECURITY_PROTOCOL = "SASL_SSL";
+    public static String SASL_JAAS_CONFIG = "org.apache.kafka.common.security.plain.PlainLoginModule required username='*************' password='*************';";
+    public static String SASL_MECHANISM = "PLAIN";
 
 }
--- src/main/java/io/woolford/SnowplowStreams.java
+++ src/main/java/io/woolford/SnowplowStreams.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.snowplowanalytics.snowplow.analytics.scalasdk.Event;
 import com.snowplowanalytics.snowplow.analytics.scalasdk.ParsingError;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -29,9 +30,12 @@ public class SnowplowStreams {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, KafkaConstants.APPLICATION_ID);
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConstants.KAFKA_BROKERS);
+        props.put(StreamsConfig.SECURITY_PROTOCOL_CONFIG, KafkaConstants.SECURITY_PROTOCOL);
+        props.put(SaslConfigs.SASL_JAAS_CONFIG, KafkaConstants.SASL_JAAS_CONFIG);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(SaslConfigs.SASL_MECHANISM, KafkaConstants.SASL_MECHANISM);
 
         final StreamsBuilder builder = new StreamsBuilder();
 
@@ -61,7 +65,7 @@ public class SnowplowStreams {
                 logger.info(json);
                 return json;
             }
-        }).to("snowplow-enriched-json-good");
+        }).to("snowplow-enriched-good-json");
 
         // run it
         final Topology topology = builder.build();
